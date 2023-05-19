165da4fa1e4193153c0f05e5725749a42a33a39d
Hüseyin Yetişir <huseynyetisir@gmail.com>
2023-05-19 20:31:02 +0300

    Added all comments and readme file

---
+++ Readme.md
@@ -0,0 +1,63 @@
+# Real Estate Management Application
+
+This is a simple real estate management application that allows you to create and manage houses, villas, and summer houses. You can calculate total prices, average areas, and filter houses based on the number of rooms.
+
+## Getting Started
+
+These instructions will guide you on how to run the application on your local machine.
+
+### Prerequisites
+
+- Java Development Kit (JDK) 8 or above
+- Apache Maven
+
+### Installing
+
+1. Clone the repository to your local machine:
+
+git clone https://github.com/Huseyinyetisir/EstateManagement.git
+
+
+2. Navigate to the project directory:
+
+cd real-estate-management
+
+
+3. Build the project using Maven:
+
+mvn clean install
+
+
+### Running the Application
+
+1. Execute the following command to run the application:
+
+java -jar target/real-estate-management.jar
+
+
+2. The application will display a menu in the terminal. Use the provided options to interact with the application.
+
+## Usage
+
+Upon running the application, you will be presented with a menu that allows you to perform various operations:
+
+### Real Estate Service Menu:
+    1. Get total price of houses
+    2. Get total price of villas
+    3. Get total price of summer houses
+    4. Get total price of all houses
+    5. Get average area of houses
+    6. Get average area of villas
+    7. Get average area of summer houses
+    8. Get average area of all houses
+    9. Filter houses by room and living room count
+    0. Exit
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvement, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT)
+
--- src/main/java/org/example/service/EstateService.java
+++ src/main/java/org/example/service/EstateService.java
@@ -21,7 +21,9 @@ public class EstateService {
 
         Random random = new Random();
 
-        // Create instances of House using HouseFactory
+        /*
+         * Create instances of House using EstateFactory
+         */
         for (int i = 0; i < 3; i++) {
             double price = 100000 + random.nextInt(400000);
             double area = 80 + random.nextInt(221);
@@ -29,7 +31,9 @@ public class EstateService {
             houseList.add(EstateFactory.createHouse(EstateType.HOUSE, price, area, numberOfRooms));
         }
 
-        // Create instances of Villa using HouseFactory
+        /*
+         *Create instances of Villa using EstateFactory
+         */
         for (int i = 0; i < 3; i++) {
             double price = 500000 + random.nextInt(500000);
             double area = 200 + random.nextInt(101);
@@ -37,7 +41,10 @@ public class EstateService {
             villaList.add(EstateFactory.createHouse(EstateType.VILLA, price, area, numberOfRooms));
         }
 
-        // Create instances of SummerHouse using HouseFactory
+
+        /*
+         * Create instances of SummerHouse using EstateFactory
+         */
         for (int i = 0; i < 3; i++) {
             double price = 200000 + random.nextInt(200000);
             double area = 60 + random.nextInt(61);
@@ -46,106 +53,139 @@ public class EstateService {
         }
     }
 
-        public List<Estate> getHouseList() {
-            return houseList;
-        }
-
-        public List<Estate> getVillaList() {
-            return villaList;
-        }
-
-        public List<Estate> getSummerHouseList() {
-            return summerHouseList;
-        }
-
-        public double getTotalPriceOfHouses () {
-            double total = 0;
-            for (Estate house : houseList) {
-                total += house.getPrice();
-            }
-            return total;
-        }
-
-        public double getTotalPriceOfVillas () {
-            double total = 0;
-            for (Estate villa : villaList) {
-                total += villa.getPrice();
-            }
-            return total;
-        }
-
-        public double getTotalPriceOfSummerHouses () {
-            double total = 0;
-            for (Estate summerHouse : summerHouseList) {
-                total += summerHouse.getPrice();
-            }
-            return total;
-        }
-
-        public double getTotalPriceOfAllEstates () {
-            return getTotalPriceOfHouses() + getTotalPriceOfVillas() + getTotalPriceOfSummerHouses();
-        }
-
-        public double getAverageAreaOfHouses() {
-            double totalArea = 0;
-            for (Estate house : houseList) {
-                totalArea += house.getArea();
-            }
-            return totalArea / houseList.size();
-        }
-
-        public double getAverageAreaOfVillas() {
-            double totalArea = 0;
-            for (Estate villa : villaList) {
-                totalArea += villa.getArea();
-            }
-            return totalArea / villaList.size();
-        }
-
-        public double getAverageAreaOfSummerHouses() {
-            double totalArea = 0;
-            for (Estate summerHouse : summerHouseList) {
-                totalArea += summerHouse.getArea();
-            }
-            return totalArea / summerHouseList.size();
-        }
-        public double getTotalAreaOfHouses() {
-            double totalArea = 0;
-            for (Estate house : houseList) {
-                totalArea += house.getArea();
-            }
-            return totalArea ;
-        }
-
-        public double getTotalAreaOfVillas() {
-            double totalArea = 0;
-            for (Estate villa : villaList) {
-                totalArea += villa.getArea();
-            }
-            return totalArea;
-        }
-
-        public double getTotalAreaOfSummerHouses() {
-            double totalArea = 0;
-            for (Estate summerHouse : summerHouseList) {
-                totalArea += summerHouse.getArea();
-            }
-            return totalArea;
-        }
-
-        public double getAverageAreaOfAllHouses () {
-            int totalNumOfHouses = houseList.size() + villaList.size() + summerHouseList.size();
-            double totalArea = getTotalAreaOfHouses() + getTotalAreaOfVillas() + getTotalAreaOfSummerHouses();
-            return totalArea / totalNumOfHouses;
-        }
-
-        public List<Estate> filterHousesByRoomAndLivingRoom (int numberOfRooms){
-            List<Estate> filteredHouses = new ArrayList<>();
-            for (Estate house : houseList) {
-                if (house.getNumberOfRooms() == numberOfRooms) {
-                    filteredHouses.add(house);
-                }
-            }
-            return filteredHouses;
-        }
+    /*
+    * getHouseList method returns house list
+    * */
+    public List<Estate> getHouseList() {
+        return houseList;
     }
+    /*
+     * getVillaList method returns villa list
+     * */
+    public List<Estate> getVillaList() {
+        return villaList;
+    }
+    /*
+     * getSummerHouseList method returns Summer House list
+     * */
+    public List<Estate> getSummerHouseList() {
+        return summerHouseList;
+    }
+
+    /*
+    * getTotalPriceOfHouses method returns Total Price Of Houses
+    * */
+    public double getTotalPriceOfHouses () {
+        double total = 0;
+        for (Estate house : houseList) {
+            total += house.getPrice();
+        }
+        return total;
+    }
+    /*
+     * getTotalPriceOfVillas method returns Total Price Of Villas
+     * */
+    public double getTotalPriceOfVillas () {
+        double total = 0;
+        for (Estate villa : villaList) {
+            total += villa.getPrice();
+        }
+        return total;
+    }
+    /*
+     * getTotalPriceOfSummerHouses method returns Total Price Of Summer Houses
+     * */
+    public double getTotalPriceOfSummerHouses () {
+        double total = 0;
+        for (Estate summerHouse : summerHouseList) {
+            total += summerHouse.getPrice();
+        }
+        return total;
+    }
+    /*
+     * getTotalPriceOfAllEstates method returns Total Price Of All Estates
+     * */
+    public double getTotalPriceOfAllEstates () {
+        return getTotalPriceOfHouses() + getTotalPriceOfVillas() + getTotalPriceOfSummerHouses();
+    }
+    /*
+     * getAverageAreaOfHouses returns average area Of Houses
+     * */
+    public double getAverageAreaOfHouses() {
+        double totalArea = 0;
+        for (Estate house : houseList) {
+            totalArea += house.getArea();
+        }
+        return totalArea / houseList.size();
+    }
+    /*
+     * getTotalPriceOfSummerHouses method returns average area Of Villas
+     * */
+    public double getAverageAreaOfVillas() {
+        double totalArea = 0;
+        for (Estate villa : villaList) {
+            totalArea += villa.getArea();
+        }
+        return totalArea / villaList.size();
+    }
+    /*
+     * getTotalPriceOfSummerHouses method returns average area Of Summer Houses
+     * */
+    public double getAverageAreaOfSummerHouses() {
+        double totalArea = 0;
+        for (Estate summerHouse : summerHouseList) {
+            totalArea += summerHouse.getArea();
+        }
+        return totalArea / summerHouseList.size();
+    }
+    /*
+     * getTotalPriceOfSummerHouses method returns Total Area Of Houses
+     * */
+    public double getTotalAreaOfHouses() {
+        double totalArea = 0;
+        for (Estate house : houseList) {
+            totalArea += house.getArea();
+        }
+        return totalArea ;
+    }
+    /*
+     * getTotalPriceOfSummerHouses method returns Total Area Of Villas
+     * */
+    public double getTotalAreaOfVillas() {
+        double totalArea = 0;
+        for (Estate villa : villaList) {
+            totalArea += villa.getArea();
+        }
+        return totalArea;
+    }
+    /*
+     * getTotalPriceOfSummerHouses method returns Total Area Of Summer Houses
+     * */
+    public double getTotalAreaOfSummerHouses() {
+        double totalArea = 0;
+        for (Estate summerHouse : summerHouseList) {
+            totalArea += summerHouse.getArea();
+        }
+        return totalArea;
+    }
+    /*
+     * getTotalPriceOfSummerHouses method returns Total Area Of All Estates
+     * */
+    public double getAverageAreaOfAllHouses () {
+        int totalNumOfHouses = houseList.size() + villaList.size() + summerHouseList.size();
+        double totalArea = getTotalAreaOfHouses() + getTotalAreaOfVillas() + getTotalAreaOfSummerHouses();
+        return totalArea / totalNumOfHouses;
+    }
+    /*
+     * getTotalPriceOfSummerHouses method returns Filtered Houses by Number Of Rooms
+     * */
+    public List<Estate> filterHousesByRoomAndLivingRoom (int numberOfRooms){
+        List<Estate> filteredHouses = new ArrayList<>();
+        for (Estate house : houseList) {
+            if (house.getNumberOfRooms() == numberOfRooms) {
+                filteredHouses.add(house);
+            }
+        }
+        return filteredHouses;
+    }
+}
